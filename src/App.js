5329710d99b0877da95cdeffe3f71ff6ce9da21c
Bappy71 <19103076@iubat.edu>
2022-10-16 04:25:24 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <person></person>
+      <person></person>
+    </div>
+  );
+}
+
+function person() {
+  return <h1>Arafat Bappy</h1>
+}
+
+export default App;
