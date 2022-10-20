775d37968af8d54b53d22b8160497070db360a75
Bappy71 <19103076@iubat.edu>
2022-10-20 09:34:49 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const Name = ['rofiq', 'jabbar', 'shafiq', 'salam']
   return (
     <div className="App">
-      <Friend phone = '01849675876' Address= 'Dhaka'></Friend>
-      <Friend phone = '01749675876' Address= 'RongPur'></Friend>
-      <Friend phone = '01949675876' Address= 'Feni'></Friend>
+      <a href="https://www.google.com/">Google</a>
+<Person name= {Name[0]} play="ludu"></Person>
+<Person name={Name[1]} play="football"></Person>
+<Person name="Rabbi" play="Carrom"></Person>
+<Person name="shajib" play="COC"></Person>
     </div>
   );
 }
-/*<a href="https://www.google.com/">Google</a>
-<Person name="Sabbir" play="ludu"></Person>
-<Person name="bappy" play="football"></Person>
-<Person name="Rabbi" play="Carrom"></Person>
-<Person name="shajib" play="COC"></Person>*/
+/*<Friend phone = '01849675876' Address= 'Dhaka'></Friend>
+      <Friend phone = '01749675876' Address= 'RongPur'></Friend>
+      <Friend phone = '01949675876' Address= 'Feni'></Friend>
+
+*/
 
 function Person(props) {
   console.log(props);
